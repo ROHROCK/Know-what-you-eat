576b0f767ef9f462510bac551c2cb331938e0e08
ROHROCK <rohitsa40@gmail.com>
2020-05-01 12:38:18 +0530

    Added Jest App test

--- know-what-you-eat/src/App.test.js
+++ know-what-you-eat/src/App.test.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import Login from "./components/Login";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+afterEach(cleanup);
+
+it("should take a snapshot", () => {
+  const { asFragment } = render(<Login />);
+
+  expect(asFragment(<Login />)).toMatchSnapshot();
 });
