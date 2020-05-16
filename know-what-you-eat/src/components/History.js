8fea6e43d524832a753734439c0fefd00911f291
ROHROCK <rohitsa40@gmail.com>
2020-05-16 11:55:30 +0530

    History

--- know-what-you-eat/src/components/History.js
+++ know-what-you-eat/src/components/History.js
@@ -3,7 +3,7 @@ import Axios from "axios";
 import NavbarComponent from "./NavbarComponent";
 import "./css/history.css";
 import fire from "./css/assests/fire.png";
-import {Bar,Pie, Line} from 'react-chartjs-2';
+import {Bar,Line,Pie} from 'react-chartjs-2';
 
 class History extends Component {
   constructor(props) {
@@ -12,27 +12,38 @@ class History extends Component {
       loading: false,
       data: [],
       chartData:{
-        labels:['Monday','Tuesday','Wensday','Thursday','Friday'],
-        datasets:[{
-          label:'Calorie',
-          data:[
-            250,
-            100,
-            300,
-            1000,
-            230
-          ],
-          backgroundColor:[
-            'rgba(255,99,132,0.6)',
-            'rgba(54,162,235,0.6)',
-            'rgba(255,206,86,0.6)',
-            'rgba(75,192,192,0.6)',
-            'rgba(153,102,255,0.6)',
-            'rgba(255,159,64,0.6)',
-            'rgba(255,99,132,0.6)'
-          ]
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
         }]
-      }
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
     };
     this.fetchHistory = this.fetchHistory.bind(this);
   }
